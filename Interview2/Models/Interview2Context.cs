b386cc677c3eb0de1904a08dd32b19c5ee0fd223
haritharan <haritharanr@gmail.com>
2020-01-24 21:46:23 +0000

    On branch master
     Initial commit
    
     Test answers for now-casting

---
+++ Interview2/Models/Interview2Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Interview2.Models
+{
+    public class Interview2Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Interview2Context() : base("name=Interview2Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Interview2.Models.User> Users { get; set; }
+    }
+}
---
+++ Interview2/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Interview2.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please provide your first name!")]
+        public string First_Name { get; set; }
+        [Required(ErrorMessage = "Please provide your last name!")]
+        public string Last_Name { get; set; }
+        [Required(ErrorMessage = "Please provide your email address!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please provide your mobile number!")]
+        public string MobileNumber { get; set; }
+        [Required(ErrorMessage = "Please provide a valid date of birth!")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        
+        public DateTime? Modified { get; set; }
+    }
+}
